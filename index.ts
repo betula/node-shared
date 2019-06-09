68039769e985529ab9f5ea18fea0fd26471bac90
betula <mail@betula.co>
2019-06-10 01:44:05 +0800

    Add redirect function

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import "reflect-metadata";
 
+type Type<T = any> = new (...args: any) => T;
+
 const instances = new Map();
+const redirects = new Map();
 
 export function provide(target: object, propertyKey: string): any {
   const Class = Reflect.getMetadata("design:type", target, propertyKey);
@@ -20,7 +23,8 @@ export function provide(target: object, propertyKey: string): any {
   };
 }
 
-export function resolve<T>(Class: new (...args: any) => T): T {
+export function resolve<T>(Class: Type<T>): T {
+  Class = redirects.get(Class) || Class;
   let instance = instances.get(Class);
   if (!instance) {
     instances.set(Class, (instance = new Class()));
@@ -28,6 +32,17 @@ export function resolve<T>(Class: new (...args: any) => T): T {
   return instance;
 }
 
+export function redirect(from: Type, to: Type): void;
+export function redirect(...pairs: Type[][]): void;
+export function redirect(...args: any[]): void {
+  if (Array.isArray(args[0])) {
+    (args as Type[][]).forEach((pair) => redirect(pair[0], pair[1]));
+  } else {
+    redirects.set(args[0], args[1]);
+  }
+}
+
 export function reset() {
   instances.clear();
+  redirects.clear();
 }
