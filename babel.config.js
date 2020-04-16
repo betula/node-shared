5bc2c74562a94c6f99fe8595f1bdeb05d0b44fea
betula <mail@betula.co>
2020-04-16 10:41:10 +0700

    Try to fix Travis build

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,7 @@ module.exports = {
     ["@babel/preset-env", {targets: {node: "current"}}],
     "@babel/preset-typescript",
   ],
-  plugins: []
+  plugins: [
+    [ "@babel/plugin-proposal-class-properties", { "loose": true } ]
+  ]
 };
